a416978dc43d431dbab8c5659c50d66bcbd17ba6
Anthony_Romagnolo <anthonyroma741@gmail.com>
2020-11-10 16:56:52 -0500

    added student resources, then made nav bar for students_index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_10_172136) do
+ActiveRecord::Schema.define(version: 2020_11_10_210844) do
+
+  create_table "students", force: :cascade do |t|
+    t.string "name"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_students_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -25,4 +33,5 @@ ActiveRecord::Schema.define(version: 2020_11_10_172136) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "students", "users"
 end
