be0909d1d5c108c99daffe38eec0b1d18eeddc1b
Anthony_Romagnolo <anthonyroma741@gmail.com>
2020-11-12 14:05:24 -0500

    added show, index and error message for Active record not found

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,4 +1,17 @@
 class StudentsController < ApplicationController
+    before_action :authenticate_user!
+    
     def index
+        @students = current_user.students
+    end
+
+    def show
+        @student = current_user.students.find(params[:id]) 
+    end
+
+    private
+    
+    def set_student
+       @student = current_user.students.find(params[:id]) 
     end
 end
