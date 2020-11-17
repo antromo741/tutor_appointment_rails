460e448114107d3eff752bfffcaad4848d11bd44
Anthony_Romagnolo <anthonyroma741@gmail.com>
2020-11-17 13:20:04 -0500

    was able to add the tutor login and succesfully login, set up tutor appointments  index route

--- app/controllers/tutor/appointments_controller.rb
+++ app/controllers/tutor/appointments_controller.rb
@@ -1,2 +1,8 @@
 class Tutor::AppointmentsController < ApplicationController
+    before_action :authenticate_tutor!
+    
+    def index
+        @appointments = current_tutor.appointments
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
     resources :appointments, only: [:index, :new]
   end
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  root to: 'students#index', constraints: lambda { |request| request.env['warden'].user.class.name == 'User'}, as: "user_root"
+  root to: 'students#index', constraints: lambda { |request| request.env['warden'].user.class.name == 'User' }, as: "user_root"
   root to: 'tutor/appointments#index', constraints: lambda { |request| request.env['warden'].user.class.name == 'Tutor' }, as: "tutor_root"
   
   namespace :tutor do
