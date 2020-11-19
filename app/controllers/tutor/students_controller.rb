7d6d09f14b14f6e8728e9b8bd7fb53e63f031f61
Anthony_Romagnolo <anthonyroma741@gmail.com>
2020-11-19 16:17:28 -0500

    Met requirments, ubt trying to set up tutor::student and tutor::appointment controller routes, very hard and confusing

--- app/controllers/tutor/students_controller.rb
+++ app/controllers/tutor/students_controller.rb
@@ -1,2 +1,18 @@
 class Tutor::StudentsController < ApplicationController
+    before_action :authenticate_tutor!
+    before_action :set_student, only: [:show]
+    def index
+        @students = Student.all
+    end
+
+    def show
+
+    end
+    
+    private
+    
+    def set_student
+       @student = Student.find(params[:id]) 
+    end
+
 end
