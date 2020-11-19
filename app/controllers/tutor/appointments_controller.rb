9bd1ff691983b59db25d8f90c93d930d156a4db4
Anthony_Romagnolo <anthonyroma741@gmail.com>
2020-11-19 17:00:41 -0500

    I am unable to setup an appointment as a tutor, I cant figure out why when I use tutorrootpath, I get redirected to the regular controller instead of the tutor:appointment controller, just going to spruce up views and submit project, wasting too much time trying to get this too work

--- app/controllers/tutor/appointments_controller.rb
+++ app/controllers/tutor/appointments_controller.rb
@@ -2,7 +2,81 @@ class Tutor::AppointmentsController < ApplicationController
     before_action :authenticate_tutor!
     
     def index
-        @appointments = current_tutor.appointments
-    end
-
+        @student = current_tutor.students.find_by_id(params[:student_id])
+        @tutor = Tutor.find_by_id(params[:tutor_id])
+        if @student
+            @appointments = @student.appointments
+        elsif @tutor
+            @appointments = current_tutor.appointments.by_tutor(@tutor)
+        else
+            @appointments = current_tutor.appointments
+            end
+            filter_options
+        end
+    
+        def show
+            
+        end
+    
+        def new
+            @student = current_tutor.students.find_by_id(params[:student_id])
+            @tutor = Tutor.find_by_id(params[:tutor_id])
+            if @student
+               @appointment = @student.appointments.build
+            elsif @tutor
+                @appointment = @tutor.appointments.build
+            else
+            @appointment = Appointment.new
+            end
+        end
+    
+        def create
+            @appointment = Appointment.new(appointment_params)
+            if @appointment.save
+                redirect_to appointment_path(@appointment)
+            else
+                render :new
+            end
+        end
+        
+        def edit
+    
+        end
+    
+        def update
+            if @appointment.update(appointment_params)
+                redirect_to appointment_path(@appointment)
+            else 
+                render :edit
+            end
+        end
+    
+        def destroy
+            @appointment.destroy
+            redirect_to appointments_path
+        end
+    
+        private
+        
+        def set_appointment
+           @appointment = current_tutor.appointments.find(params[:id]) 
+        end
+        
+        #using the scopes to filter the appointments
+        def filter_options 
+            if params[:filter_by_time] == "upcoming"
+              @appointments = @appointments.upcoming
+            elsif params[:filter_by_time] == "past"
+              @appointments = @appointments.past
+            end
+            if params[:sort] == "most_recent"
+              @appointments = @appointments.most_recent
+            elsif params[:sort] == "longest_ago" 
+              @appointments = @appointments.longest_ago
+            end
+        end
+    
+        def appointment_params
+            params.require(:appointment).permit(:location, :start_time, :end_time, :tutor_id, :student_id)
+        end
 end
